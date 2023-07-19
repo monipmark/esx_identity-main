2485c5b4368e34c83f50e095333651f34195924a
monipmark <90512420+monipmark@users.noreply.github.com>
2023-07-20 00:31:53 +0700

    Update esx_identity.sql

--- esx_identity.sql
+++ esx_identity.sql
@@ -7,6 +7,17 @@ ALTER TABLE `users`
 	ADD COLUMN `sex` VARCHAR(10) NULL DEFAULT '',
 	ADD COLUMN `height` VARCHAR(5) NULL DEFAULT ''
 ;
+
+CREATE TABLE IF NOT EXISTS `characters` (
+  `identifier` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `dateofbirth` varchar(100) NOT NULL,
+  `sex` int(11) NOT NULL,
+  `height` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 ALTER TABLE `characters`
 	ADD COLUMN `identifier` VARCHAR(50) NULL DEFAULT '',
 	ADD COLUMN `firstname` VARCHAR(50) NULL DEFAULT '',
